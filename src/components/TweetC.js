e6e0a8a311d12eae3ad34a53db72a10c27a6961a
siheon <korimse@gmail.com>
2021-01-28 14:48:21 +0900

    login

---
+++ src/components/TweetC.js
@@ -0,0 +1,58 @@
+import { dbService } from "fbase";
+import React, { useState } from "react";
+
+const TweetC = ({ tweetObj, isOwner }) => {
+    const [editing, setEditing] = useState(false);
+    const [newTweet, setnewTweet] = useState(tweetObj.text);
+    const onDeleteClick = async () => {
+        const ok = window.confirm("Are you sure you want to delete this tweet??");
+        console.log(ok)
+        if (ok) {
+            await dbService.doc(`tweets/${tweetObj.id}`).delete();
+        }
+    };
+    const toggleEditing = () => setEditing(prev => !prev);
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        await dbService.doc(`tweets/${tweetObj.id}`).update({
+            text: newTweet,
+        });
+        setEditing(false);
+    }
+    const onChange = (event) => {
+        const {
+            target: { value },
+        } = event;
+        setnewTweet(value);
+    }
+    return (
+        <div>
+            {editing ? (
+                <>
+                    <form onSubmit={onSubmit}>
+                        <input
+                            type="text"
+                            placeholder="Edit your nweet"
+                            value={newTweet}
+                            required
+                            onChange={onChange}
+                        />
+                        <input type="submit" value="Update Tweet" />
+                    </form>
+                    <button onClick={toggleEditing}>Cancel</button>
+                </>
+            ) : (
+                    <>
+                        <h4>{tweetObj.text}</h4>
+                        {isOwner && (
+                            <>
+                                <button onClick={onDeleteClick}>Delete Tweet</button>
+                                <button onClick={toggleEditing}>Edit Tweet</button>
+                            </>
+                        )}
+                    </>
+                )}
+        </div>
+    );
+};
+export default TweetC;
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,4 +1,47 @@
-import React from "react";
+import { dbService } from "fbase";
+import React, { useEffect, useState } from "react";
+import TweetC from "components/TweetC";
 
-const Home = () => <span>Home</span>;
+const Home = ({ userObj }) => {
+    const [Tweet, setTweet] = useState("");
+    const [tweets, settweets] = useState([]);
+    useEffect(() => {
+        dbService.collection("tweets").onSnapshot(snapshot => {
+            const tweetArray = snapshot.docs.map((doc) => ({
+                id: doc.id,
+                ...doc.data(),
+            }));
+            settweets(tweetArray);
+        });
+    }, []);
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        await dbService.collection("tweets").add({
+            text: Tweet,
+            createAt: Date.now(),
+            creatorID: userObj.uid,
+        });
+        setTweet("");
+    };
+    const onChange = (event) => {
+        const { target: { value }, } = event;
+        setTweet(value);
+    }
+    return (
+        <div>
+            <form onSubmit={onSubmit}>
+                <input value={Tweet} onChange={onChange} type="text" placeholder="what's on your mind?" maxLength={120} />
+                <input type="submit" placeholder="Tweet" />
+            </form>
+            <div>
+                {tweets.map(Tweet => (
+                    <TweetC key={Tweet.id}
+                        tweetObj={Tweet}
+                        isOwner={Tweet.creatorID === userObj.uid}
+                    />
+                ))}
+            </div>
+        </div>
+    )
+}
 export default Home;
