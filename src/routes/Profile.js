cb070d165f464931681c1f0d2bd540cd635ca1cb
siheon <korimse@gmail.com>
2021-02-07 00:21:48 +0900

    profile

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,22 +1,37 @@
-import { authService, dbService } from "fbase";
-import React, { useEffect } from "react";
+import { authService } from "fbase";
+import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
 
-export default ({ userObj }) => {
+export default ({ refreshUser, userObj }) => {
     const history = useHistory();
+    const [newDisplayName, setNewDisplayName] = useState(userObj.displayName);
     const onLogOutClick = () => { authService.signOut(); history.push("/"); };
-    const getMyTweets = async () => {
-        const tweets = await dbService
-            .collection("tweets")
-            .where("creatorID", "==", userObj.uid)
-            .get();
-        console.log(tweets.docs.map((doc) => doc.data()));
+    const onChange = (event) => {
+        const {
+            target: { value },
+        } = event;
+        setNewDisplayName(value);
     };
-    useEffect(() => {
-        getMyTweets();
-    }, []);
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        if (userObj.displayName !== newDisplayName) {
+            await userObj.updateProfile({
+                displayName: newDisplayName,
+            });
+            refreshUser();
+        }
+    }
     return (
         <>
+            <form onSubmit={onSubmit}>
+                <input
+                    onChange={onChange}
+                    type="text"
+                    placeholder="Display name"
+                    value={newDisplayName}
+                />
+                <input type="submit" value="Update Profile" />
+            </form>
             <button onClick={onLogOutClick}>Log Out</button>
         </>);
 };
